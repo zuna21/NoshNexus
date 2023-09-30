70125f356c9bb731176e58e0b1226b7df2aff873
zuna21 <adir.zunic@gmail.com>
2023-09-30 15:05:05 +0200

    Created responsive navigation

--- owner/owner-client/src/app/_components/main-components/main-components.component.ts
+++ owner/owner-client/src/app/_components/main-components/main-components.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDrawerMode, MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { SideNavComponent } from './side-nav/side-nav.component';
 import { TopNavComponent } from './top-nav/top-nav.component';
 import {MatIconModule} from '@angular/material/icon';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-main-components',
@@ -13,6 +15,40 @@ import {MatIconModule} from '@angular/material/icon';
   templateUrl: './main-components.component.html',
   styleUrls: ['./main-components.component.css']
 })
-export class MainComponentsComponent {
+export class MainComponentsComponent implements OnInit, OnDestroy {
+  hasBackdrop: boolean = false;
+  mode: MatDrawerMode = 'side';
+  sideOpened: boolean = true;
+
+  breakPointSub: Subscription | undefined;
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit(): void {
+    this.onTabletOrSmallerDevice();
+  }
+
+  onTabletOrSmallerDevice() {
+    this.breakPointSub = this.breakpointObserver.observe('(max-width: 768px)').subscribe({
+      next: result => {
+        if (!result) return;
+        if (!result.matches) {
+          // kada je vece od tableta i mobitela
+          this.hasBackdrop = false;
+          this.mode = 'side';
+          this.sideOpened = true;
+        } else {
+          // kada je tablet ili mobitel
+          this.hasBackdrop = true;
+          this.mode = 'over';
+          this.sideOpened = false;
+        }
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.breakPointSub?.unsubscribe();
+  }
 
 }
--- owner/owner-client/src/app/_components/main-components/top-nav/top-nav.component.ts
+++ owner/owner-client/src/app/_components/main-components/top-nav/top-nav.component.ts
@@ -4,11 +4,12 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatBadgeModule } from '@angular/material/badge';
+import { RightToggleComponent } from './right-toggle/right-toggle.component';
 
 @Component({
   selector: 'app-top-nav',
   standalone: true,
-  imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule, MatBadgeModule],
+  imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule, MatBadgeModule, RightToggleComponent],
   templateUrl: './top-nav.component.html',
   styleUrls: ['./top-nav.component.css']
 })

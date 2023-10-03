56590ac628d9dc34197c02e77fcc0e539831eda9
zuna21 <adir.zunic@gmail.com>
2023-10-03 14:21:22 +0200

    Created animation and dropdown in sidenav

--- owner/owner-client/src/app/_components/main-components/side-nav/side-nav-dropdown/side-nav-dropdown.component.ts
+++ owner/owner-client/src/app/_components/main-components/side-nav/side-nav-dropdown/side-nav-dropdown.component.ts
@@ -1,26 +1,56 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ARROW_DOWN_SVG, ARROW_UP_SVG } from 'src/app/_svgs/svg';
 import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink, RouterLinkActive } from '@angular/router';
+
+const DROPDOWN_ROW_HEIGHT: number = 35;
 
 @Component({
   selector: 'app-side-nav-dropdown',
   standalone: true,
-  imports: [CommonModule, MatIconModule, RouterLink],
+  imports: [CommonModule, MatIconModule, RouterLink, RouterLinkActive],
   templateUrl: './side-nav-dropdown.component.html',
-  styleUrls: ['./side-nav-dropdown.component.css']
+  styleUrls: ['./side-nav-dropdown.component.css'],
 })
-export class SideNavDropdownComponent {
-  @Input('itemListObj') itemListObj: {name: string; url: string}[] = [];
+export class SideNavDropdownComponent implements OnInit {
+  @Input('itemListObj') itemListObj: { name: string; url: string }[] = [];
   @Input('name') name: string = '';
   arrowDownSvg: string = ARROW_DOWN_SVG;
   arrowUpSvg: string = ARROW_UP_SVG;
   isOpen: boolean = false;
+  totalDropdownHeight: string = '0px';
 
-  constructor(private iconRegistry: MatIconRegistry, private senitezer: DomSanitizer) {
-    iconRegistry.addSvgIconLiteral('arrow-down-svg', senitezer.bypassSecurityTrustHtml(this.arrowDownSvg));
-    iconRegistry.addSvgIconLiteral('arrow-up-svg', senitezer.bypassSecurityTrustHtml(this.arrowUpSvg));
+  isMainActive: boolean = false;
+
+  constructor(
+    private iconRegistry: MatIconRegistry,
+    private senitezer: DomSanitizer,
+    private activatedRoute: ActivatedRoute
+  ) {
+    iconRegistry.addSvgIconLiteral(
+      'arrow-down-svg',
+      senitezer.bypassSecurityTrustHtml(this.arrowDownSvg)
+    );
+    iconRegistry.addSvgIconLiteral(
+      'arrow-up-svg',
+      senitezer.bypassSecurityTrustHtml(this.arrowUpSvg)
+    );
   }
+
+  ngOnInit(): void {
+      this.calculateTotalDropdownHeight();
+  }
+
+
+  calculateTotalDropdownHeight() {
+    this.totalDropdownHeight =  `${this.itemListObj.length * DROPDOWN_ROW_HEIGHT}px`; 
+  }
+
+  mainIsActive(e: boolean) {
+    this.isMainActive = e;
+  }
+
+
 }

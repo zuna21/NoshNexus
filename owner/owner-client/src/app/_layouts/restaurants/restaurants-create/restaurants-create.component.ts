6cc3a0bae6241fecd4d3db6605bee240a01fd5e3
zuna21 <adir.zunic@gmail.com>
2023-10-04 14:10:08 +0200

    design in restaurant create component done

--- owner/owner-client/src/app/_layouts/restaurants/restaurants-create/restaurants-create.component.ts
+++ owner/owner-client/src/app/_layouts/restaurants/restaurants-create/restaurants-create.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { GalleryItem, ImageItem } from 'ng-gallery';
 
 @Component({
   selector: 'app-restaurants-create',
   templateUrl: './restaurants-create.component.html',
   styleUrls: ['./restaurants-create.component.css']
 })
-export class RestaurantsCreateComponent {
+export class RestaurantsCreateComponent implements OnInit {
+
+  restaurantImages: GalleryItem[] = [];
+
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon('facebook-logo', sanitizer.bypassSecurityTrustResourceUrl('assets/svg/facebook-logo.svg'));
+    iconRegistry.addSvgIcon('instagram-logo', sanitizer.bypassSecurityTrustResourceUrl('assets/svg/instagram-logo.svg'));
+    iconRegistry.addSvgIcon('globe', sanitizer.bypassSecurityTrustResourceUrl('assets/svg/globe.svg'));
+    iconRegistry.addSvgIcon('image', sanitizer.bypassSecurityTrustResourceUrl('assets/svg/image.svg'));
+    iconRegistry.addSvgIcon('images', sanitizer.bypassSecurityTrustResourceUrl('assets/svg/images.svg'));
+    iconRegistry.addSvgIcon('save', sanitizer.bypassSecurityTrustResourceUrl('assets/svg/save.svg'));
+  }
+
+  ngOnInit(): void {
+      this.loadRestaurantImages();
+  }
+
+  loadRestaurantImages() {
+    this.restaurantImages = [
+      new ImageItem({src: 'assets/img/default.png', thumb: 'assets/img/default.png'}),
+      new ImageItem({src: 'assets/img/default.png', thumb: 'assets/img/default.png'}),
+      new ImageItem({src: 'assets/img/default.png', thumb: 'assets/img/default.png'}),
+      new ImageItem({src: 'assets/img/default.png', thumb: 'assets/img/default.png'}),
+      new ImageItem({src: 'assets/img/default.png', thumb: 'assets/img/default.png'}),
+      new ImageItem({src: 'assets/img/default.png', thumb: 'assets/img/default.png'}),
+      new ImageItem({src: 'assets/img/default.png', thumb: 'assets/img/default.png'}),
+      new ImageItem({src: 'assets/img/default.png', thumb: 'assets/img/default.png'}),
+      new ImageItem({src: 'assets/img/default.png', thumb: 'assets/img/default.png'}),
+    ];
+  }
 
 }
--- owner/owner-client/src/app/app.module.ts
+++ owner/owner-client/src/app/app.module.ts
@@ -8,6 +8,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './_layouts/home/home.component';
 import { RestaurantsComponent } from './_layouts/restaurants/restaurants.component';
 import { RestaurantsCreateComponent } from './_layouts/restaurants/restaurants-create/restaurants-create.component';
+import { AngularMaterialModule } from './_modules/angular-material.module';
+import { HttpClientModule } from '@angular/common/http';
+import { GalleryModule, GALLERY_CONFIG, GalleryConfig } from 'ng-gallery';
+import { LIGHTBOX_CONFIG, LightboxConfig, LightboxModule } from 'ng-gallery/lightbox';
 
 @NgModule({
   declarations: [
@@ -20,9 +24,28 @@ import { RestaurantsCreateComponent } from './_layouts/restaurants/restaurants-c
     BrowserModule,
     AppRoutingModule,
     MainComponentsComponent,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AngularMaterialModule,
+    GalleryModule,
+    LightboxModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: GALLERY_CONFIG,
+      useValue: {
+        autoHeight: true,
+        imageSize: 'cover'
+      } as GalleryConfig
+    },
+    {
+      provide: LIGHTBOX_CONFIG,
+      useValue: {
+        keyboardShortcuts: false,
+        exitAnimationTime: 1000
+      } as LightboxConfig
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

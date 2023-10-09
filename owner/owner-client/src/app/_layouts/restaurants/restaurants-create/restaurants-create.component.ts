b83d8aef5b3c1da0911fea058aac7cfebbccb287
zuna21 <adir.zunic@gmail.com>
2023-10-09 20:28:34 +0200

    Redesign restaurant create component

--- owner/owner-client/src/app/_layouts/restaurants/restaurants-create/restaurants-create.component.ts
+++ owner/owner-client/src/app/_layouts/restaurants/restaurants-create/restaurants-create.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges, SimpleChanges, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
   FormGroup,
@@ -7,14 +7,11 @@ import {
   ReactiveFormsModule,
 } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
-import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
 import { ICountry } from 'src/app/_interfaces/ICountry';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { LightboxModule } from 'ng-gallery/lightbox';
-import { DEFAULT_RESTAURANT_IMAGES } from 'src/app/_shared/default-restaurant-images';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { ZipCodeDirective } from 'src/app/_directives/zip-code.directive';
@@ -31,12 +28,9 @@ import { MatChipsModule } from '@angular/material/chips';
     ReactiveFormsModule,
     MatSelectModule,
     MatIconModule,
-    GalleryModule,
     MatInputModule,
     MatButtonModule,
-    LightboxModule,
     MatSnackBarModule,
-    ZipCodeDirective,
     MatSlideToggleModule,
     MatChipsModule
   ],
@@ -44,12 +38,9 @@ import { MatChipsModule } from '@angular/material/chips';
   styleUrls: ['./restaurants-create.component.css'],
 })
 export class RestaurantsCreateComponent {
-  restaurantImages: GalleryItem[] = [];
   countries: ICountry[] = [];
-  profileImageURL: string = '';
-  deleteDefaultImages: boolean = true;
   progressBarValue: number = 0;
-  restaurantFormGroup: FormGroup = this.fb.group({
+  restaurantForm: FormGroup = this.fb.group({
     name: ['', Validators.required],
     country: ['', Validators.required],
     postalCode: [null, [Validators.required, Validators.minLength(5)]],
@@ -60,9 +51,7 @@ export class RestaurantsCreateComponent {
     facebookUrl: [''],
     instagramUrl: [''],
     websiteUrl: [''],
-    isActive: [false, Validators.required],
-    profilePicture: [null],
-    otherImages: [[]],
+    isActive: [false, Validators.required]
   });
 
   constructor(
@@ -73,7 +62,6 @@ export class RestaurantsCreateComponent {
 
   ngOnInit(): void {
     this.loadCountries();
-    this.loadRestaurantImages();
   }
 
 
@@ -81,70 +69,9 @@ export class RestaurantsCreateComponent {
     this.countries = structuredClone(COUNTRIES);
   }
 
-  loadRestaurantImages() {
-    this.restaurantImages = [...DEFAULT_RESTAURANT_IMAGES];
-  }
-
-  uploadProfilePicture(event: any) {
-    const inputHtml = event.target as HTMLInputElement;
-    if (!inputHtml || !inputHtml.files || inputHtml.files.length <= 0) {
-      this.snackBar.open('Please upload image', 'Ok', {
-        duration: 3000,
-        panelClass: 'info-snackbar',
-      });
-      return;
-    }
-    const file = inputHtml.files[0];
-    this.restaurantFormGroup.patchValue({
-      profilePicture: file,
-    });
-
-    this.profileImageURL = URL.createObjectURL(file);
-    this.snackBar.open('Successfully added restaurant profile image!', 'Ok', {
-      duration: 2000,
-      panelClass: 'success-snackbar',
-    });
-  }
-
-  uploadOtherPictures(event: any) {
-    const inputHtml = event.target as HTMLInputElement;
-    if (!inputHtml || !inputHtml.files || inputHtml.files.length <= 0) {
-      this.snackBar.open('Please upload images', 'Ok', {
-        duration: 3000,
-        panelClass: 'info-snackbar',
-      });
-      return;
-    }
-    const files = inputHtml.files;
-    if (this.deleteDefaultImages) this.restaurantImages = [];
-    this.deleteDefaultImages = false;
-    for (let i = 0; i < files.length; i++) {
-      const file = files[i];
-      const images = this.restaurantFormGroup.get('otherImages')?.value;
-      const newImages = [...images, file];
-      this.restaurantFormGroup.patchValue({
-        otherImages: newImages,
-      });
-
-      const createdUrl = URL.createObjectURL(file);
-      this.restaurantImages = [
-        ...this.restaurantImages,
-        new ImageItem({ src: createdUrl, thumb: createdUrl }),
-      ];
-    }
-
-    this.snackBar.open('Successfully added restaurant images!', 'Ok', {
-      duration: 2000,
-      panelClass: 'success-snackbar',
-    });
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    console.log('Desila se promjena');
-  }
 
   onSubmit() {
-    if (this.restaurantFormGroup.invalid) {
+    if (this.restaurantForm.invalid) {
       this.snackBar.open('Something went wrong.', 'Ok', {
         panelClass: 'warning-snackbar',
       });

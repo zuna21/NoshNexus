c4c118924300d2a72ff2636019397088a9f84b48
zuna21 <adir.zunic@gmail.com>
2023-10-10 22:27:00 +0200

    Can use mock data with json server

--- owner/owner-client/src/app/_layouts/restaurants/restaurants.component.ts
+++ owner/owner-client/src/app/_layouts/restaurants/restaurants.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RestaurantCardComponent } from 'src/app/_components/restaurant-card/restaurant-card.component';
 import { IRestaurantCard } from 'src/app/_interfaces/IRestaurant';
-import { RESTAURANTS_FOR_CARD } from 'src/app/fake_data/restaurant';
+import { RestaurantService } from 'src/app/_services/restaurant.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-restaurants',
@@ -11,14 +12,26 @@ import { RESTAURANTS_FOR_CARD } from 'src/app/fake_data/restaurant';
   templateUrl: './restaurants.component.html',
   styleUrls: ['./restaurants.component.css']
 })
-export class RestaurantsComponent implements OnInit {
+export class RestaurantsComponent implements OnInit, OnDestroy {
   restaurants: IRestaurantCard[] = [];
+
+  restaurantSub: Subscription | undefined;
+
+  constructor(
+    private restaurantService: RestaurantService
+  ) { }
 
   ngOnInit(): void {
     this.loadRestaurants();
   }
 
   loadRestaurants() {
-    this.restaurants = structuredClone(RESTAURANTS_FOR_CARD);
+    this.restaurantSub = this.restaurantService.getOwnerRestaurants().subscribe({
+      next: restaurants => this.restaurants = restaurants
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.restaurantSub?.unsubscribe();
   }
 }
---
+++ owner/owner-client/src/app/_services/restaurant.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
+import { IRestaurantCard } from '../_interfaces/IRestaurant';
+
+const BASE_URL: string = `${environment.apiUrl}/restaurant`;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RestaurantService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getOwnerRestaurants(): Observable<IRestaurantCard[]> {
+    return this.http.get<IRestaurantCard[]>(`${BASE_URL}/get-owner-restaurants`);
+  }
+}

4433f0e00d3cb22765d6795f7f35960f720bf092
zuna21 <adir.zunic@gmail.com>
2023-10-11 19:08:04 +0200

    Working on header in employee details

--- owner/owner-client/src/app/_layouts/employees/employees-details/employees-details.component.ts
+++ owner/owner-client/src/app/_layouts/employees/employees-details/employees-details.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-employees-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './employees-details.component.html',
   styleUrls: ['./employees-details.component.css']
 })

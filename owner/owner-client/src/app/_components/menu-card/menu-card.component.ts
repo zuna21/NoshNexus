1587a58639c553338ef80548a5c0f7b9c8b775a7
zuna21 <adir.zunic@gmail.com>
2023-10-13 10:31:16 +0200

    Created menu create component

--- owner/owner-client/src/app/_components/menu-card/menu-card.component.ts
+++ owner/owner-client/src/app/_components/menu-card/menu-card.component.ts
@@ -9,10 +9,17 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-menu-card',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule, RouterLink],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterLink,
+  ],
   templateUrl: './menu-card.component.html',
-  styleUrls: ['./menu-card.component.css']
+  styleUrls: ['./menu-card.component.css'],
 })
 export class MenuCardComponent {
   @Input('menu') menu: IMenuCard | undefined;
+  @Input('disable') disable: boolean = false;
 }
--- owner/owner-client/src/app/_layouts/menus/menus-create/menus-create.component.ts
+++ owner/owner-client/src/app/_layouts/menus/menus-create/menus-create.component.ts
@@ -1,13 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { IRestaurantSelect } from 'src/app/_interfaces/IRestaurant';
+import { RestaurantService } from 'src/app/_services/restaurant.service';
+import { Subscription } from 'rxjs';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-menus-create',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './menus-create.component.html',
-  styleUrls: ['./menus-create.component.css']
+  styleUrls: ['./menus-create.component.css'],
 })
-export class MenusCreateComponent {
+export class MenusCreateComponent implements OnInit, OnDestroy {
+  restaurants: IRestaurantSelect[] = [];
+  menuForm: FormGroup = this.fb.group({
+    name: ['', Validators.required],
+    description: [''],
+    restaurant: [null, Validators.required],
+  });
 
+  restaurantSub: Subscription | undefined;
+
+  constructor(
+    private restaurantService: RestaurantService,
+    private fb: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.getRestaurants();
+  }
+
+  getRestaurants() {
+    this.restaurantSub = this.restaurantService
+      .getOwnerRestaurantsForSelect()
+      .subscribe({
+        next: (restaurants) => {
+          this.restaurants = restaurants;
+        },
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.restaurantSub?.unsubscribe();
+  }
+
+  onSubmit() {
+    if (!this.menuForm.valid) return;
+    console.log(this.menuForm.value);
+  }
 }

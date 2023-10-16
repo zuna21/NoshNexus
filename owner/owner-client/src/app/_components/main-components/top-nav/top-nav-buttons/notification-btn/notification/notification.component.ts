88c043bfcc4af1856f1836888c825d0a16e74458
zuna21 <adir.zunic@gmail.com>
2023-10-16 22:44:58 +0200

    Added ripple efect

--- owner/owner-client/src/app/_components/main-components/top-nav/top-nav-buttons/notification-btn/notification/notification.component.ts
+++ owner/owner-client/src/app/_components/main-components/top-nav/top-nav-buttons/notification-btn/notification/notification.component.ts
@@ -2,13 +2,16 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { INotification } from 'src/app/_interfaces/INotification';
+import { MatRippleModule } from '@angular/material/core';
+
 
 @Component({
   selector: 'app-notification',
   standalone: true,
   imports: [
     CommonModule,
-    MatIconModule
+    MatIconModule,
+    MatRippleModule
   ],
   templateUrl: './notification.component.html',
   styleUrls: ['./notification.component.css']

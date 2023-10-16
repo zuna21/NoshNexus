dcf7c53c989f718786b5034d12f3d1226e8984c9
zuna21 <adir.zunic@gmail.com>
2023-10-16 11:11:27 +0200

    Created design for account-edit

--- owner/owner-client/src/app/_layouts/account/account-edit/account-edit.component.ts
+++ owner/owner-client/src/app/_layouts/account/account-edit/account-edit.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import {MatDatepickerModule} from '@angular/material/datepicker'; 
+import {MatNativeDateModule} from '@angular/material/core';
+import { ImageWithDeleteComponent } from 'src/app/_components/image-with-delete/image-with-delete.component';
 
 @Component({
   selector: 'app-account-edit',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    ImageWithDeleteComponent
+  ],
   templateUrl: './account-edit.component.html',
   styleUrls: ['./account-edit.component.css']
 })

a047948eed03dcde0d4452ff8139129573626983
zuna21 <adir.zunic@gmail.com>
2023-10-16 09:23:45 +0200

    Created profile header

---
+++ owner/owner-client/src/app/_interfaces/IProfileHeader.ts
@@ -0,0 +1,8 @@
+export interface IProfileHeader {
+  id: string;
+  profileImage: string;
+  backgroundImage: string;
+  firstName: string;
+  lastName: string;
+  username: string;
+}
--- owner/owner-client/src/app/_layouts/account/account.component.ts
+++ owner/owner-client/src/app/_layouts/account/account.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProfileHeaderComponent } from 'src/app/_components/profile-header/profile-header.component';
 
 @Component({
   selector: 'app-account',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ProfileHeaderComponent
+  ],
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css']
 })

fb984516917d77152a85f2762774e108c4651e0c
zuna21 <adir.zunic@gmail.com>
2023-10-17 11:08:15 +0200

    Changed svg for google's svgs

--- owner/owner-client/src/app/_layouts/home/home.component.ts
+++ owner/owner-client/src/app/_layouts/home/home.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FolderWindowComponent } from 'src/app/_components/folder-window/folder-window.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FolderWindowComponent
+  ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })

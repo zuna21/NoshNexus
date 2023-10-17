6464af8d90764cf82968f253fe6ad0fff2595042
zuna21 <adir.zunic@gmail.com>
2023-10-17 15:49:08 +0200

    Working on messages

--- owner/owner-client/src/app/_components/main-components/top-nav/top-nav-buttons/top-nav-buttons.component.ts
+++ owner/owner-client/src/app/_components/main-components/top-nav/top-nav-buttons/top-nav-buttons.component.ts
@@ -2,6 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotificationBtnComponent } from './notification-btn/notification-btn.component';
 import { AccountBtnComponent } from './account-btn/account-btn.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { RouterLink } from '@angular/router';
+import { MessageBtnComponent } from './message-btn/message-btn.component';
 
 @Component({
   selector: 'app-top-nav-buttons',
@@ -9,7 +14,12 @@ import { AccountBtnComponent } from './account-btn/account-btn.component';
   imports: [
     CommonModule,
     NotificationBtnComponent,
-    AccountBtnComponent
+    AccountBtnComponent,
+    MatButtonModule, 
+    MatIconModule,
+    MatMenuModule,
+    RouterLink,
+    MessageBtnComponent
   ],
   templateUrl: './top-nav-buttons.component.html',
   styleUrls: ['./top-nav-buttons.component.css']

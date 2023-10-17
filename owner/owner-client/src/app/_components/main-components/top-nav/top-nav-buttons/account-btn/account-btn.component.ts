a092e8872af44749aa16bae9609a64c6dc81c81f
zuna21 <adir.zunic@gmail.com>
2023-10-17 15:05:37 +0200

    Added account btn

---
+++ owner/owner-client/src/app/_components/main-components/top-nav/top-nav-buttons/account-btn/account-btn.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-account-btn',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatProgressSpinnerModule,
+    MatTooltipModule,
+    MatMenuModule,
+    MatIconModule,
+    RouterLink
+  ],
+  templateUrl: './account-btn.component.html',
+  styleUrls: ['./account-btn.component.css'],
+})
+export class AccountBtnComponent {
+  isProfileImageLoading: boolean = true;
+}
--- owner/owner-client/src/app/_components/main-components/top-nav/top-nav-buttons/top-nav-buttons.component.ts
+++ owner/owner-client/src/app/_components/main-components/top-nav/top-nav-buttons/top-nav-buttons.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotificationBtnComponent } from './notification-btn/notification-btn.component';
+import { AccountBtnComponent } from './account-btn/account-btn.component';
 
 @Component({
   selector: 'app-top-nav-buttons',
   standalone: true,
   imports: [
     CommonModule,
-    NotificationBtnComponent
+    NotificationBtnComponent,
+    AccountBtnComponent
   ],
   templateUrl: './top-nav-buttons.component.html',
   styleUrls: ['./top-nav-buttons.component.css']

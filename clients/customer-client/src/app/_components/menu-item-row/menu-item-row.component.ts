7716adad73705dc0cdc923f922a05d7a57972a65
zuna21 <adir.zunic@gmail.com>
2023-12-07 21:09:58 +0100

    Removed image gallery from menu item row component

--- clients/customer-client/src/app/_components/menu-item-row/menu-item-row.component.ts
+++ clients/customer-client/src/app/_components/menu-item-row/menu-item-row.component.ts
@@ -1,11 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import {MatExpansionModule} from '@angular/material/expansion'; 
 import { MatButtonModule } from '@angular/material/button';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
-import { LightboxModule } from 'ng-gallery/lightbox';
 import { MatIconModule } from '@angular/material/icon';
 import { IMenuItemRow } from 'src/app/_interfaces/IMenuItem';
 
@@ -16,31 +14,19 @@ import { IMenuItemRow } from 'src/app/_interfaces/IMenuItem';
     CommonModule,
     MatExpansionModule,
     MatButtonModule,
-    GalleryModule,
-    LightboxModule,
     MatProgressSpinnerModule,
-    MatIconModule
+    MatIconModule,
   ],
   templateUrl: './menu-item-row.component.html',
   styleUrls: ['./menu-item-row.component.css']
 })
-export class MenuItemRowComponent implements OnInit {
+export class MenuItemRowComponent {
   @Input('menuItem') menuItem?: IMenuItemRow
 
-  imageGallery: GalleryItem[] = [];
   isImageLoading: boolean = true;
 
-  ngOnInit(): void {
-    this.generateImageGallery();
-  }
-
-  generateImageGallery() {
-    if (!this.menuItem) return;
-    for (let image of this.menuItem.images) {
-      const imageItem = new ImageItem({ src: 
-        'https://upload.wikimedia.org/wikipedia/commons/9/91/Pizza-3007395.jpg', thumb: 
-      'https://upload.wikimedia.org/wikipedia/commons/9/91/Pizza-3007395.jpg' });
-      this.imageGallery.push(imageItem);
-    }
+  onImage(event: MouseEvent) {
+    event.stopPropagation();
+    console.log('Kliknuo si na sliku');
   }
 }

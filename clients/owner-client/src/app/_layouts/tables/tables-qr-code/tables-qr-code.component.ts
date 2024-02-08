a9823a4be91017781f2cc4518f8f993ca5a3e2bc
zuna21 <adir.zunic@gmail.com>
2024-02-08 12:39:26 +0100

    Can download pdf of qr codes

--- clients/owner-client/src/app/_layouts/tables/tables-qr-code/tables-qr-code.component.ts
+++ clients/owner-client/src/app/_layouts/tables/tables-qr-code/tables-qr-code.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { QRCodeModule } from 'angularx-qrcode';
+import jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-tables-qr-code',
@@ -13,4 +14,14 @@ import { QRCodeModule } from 'angularx-qrcode';
   styleUrls: ['./tables-qr-code.component.css']
 })
 export class TablesQrCodeComponent {
+
+  async onDownload() {
+    // format a4 je 210 x 297 (width x height)
+    const table = document.getElementById('qr-wrapper');
+    console.log(table);
+    if (!table) return;
+    const doc = new jsPDF("portrait", "mm", "a4");
+    await doc.html(table);
+    doc.save("Table");
+  }
 }

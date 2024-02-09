aaca571b08647a86929f342ddaaeeee9f1f6c996
zuna21 <adir.zunic@gmail.com>
2024-02-09 07:43:18 +0100

    Added ss and new client app

--- clients/customer-client/src/app/_interfaces/ITable.ts
+++
@@ -1,4 +0,0 @@
-export interface ITable {
-    id: number;
-    name: string;
-}
--- clients/customer-client/src/app/app.config.ts
+++ clients/customer-client/src/app/app.config.ts
@@ -2,15 +2,7 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { jwtInterceptor } from './_interceptors/jwt.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), 
-    provideAnimations(),
-    provideHttpClient(
-      withInterceptors([jwtInterceptor])
-    )
-  ]
+  providers: [provideRouter(routes)]
 };

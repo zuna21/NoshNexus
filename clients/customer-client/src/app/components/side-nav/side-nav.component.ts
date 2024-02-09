3fb9e2361e302ee786b01ca070f92c082d5e9a57
zuna21 <adir.zunic@gmail.com>
2024-02-09 14:38:45 +0100

    Added router link to side nav

--- clients/customer-client/src/app/components/side-nav/side-nav.component.ts
+++ clients/customer-client/src/app/components/side-nav/side-nav.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-side-nav',
   standalone: true,
   imports: [
-    MatIconModule
+    MatIconModule,
+    RouterLink
   ],
   templateUrl: './side-nav.component.html',
   styleUrl: './side-nav.component.css'

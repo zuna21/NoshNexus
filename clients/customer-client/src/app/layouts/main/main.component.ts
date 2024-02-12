187a6d710503fbb94480a3b74af75af526137689
zuna21 <adir.zunic@gmail.com>
2024-02-12 11:34:37 +0100

    Added scroll service

--- clients/customer-client/src/app/layouts/main/main.component.ts
+++ clients/customer-client/src/app/layouts/main/main.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import { TopNavComponent } from '../../components/top-nav/top-nav.component';
 import { SideNavComponent } from '../../components/side-nav/side-nav.component';
+import { ScrollService } from './scroll.service';
 
 @Component({
   selector: 'app-main',
@@ -18,4 +19,14 @@ import { SideNavComponent } from '../../components/side-nav/side-nav.component';
 })
 export class MainComponent {
 
+  constructor(
+    private scrollService: ScrollService
+  ) {}
+
+  onScroll(event: any) {
+    // visible height + pixel scrolled >= total height 
+    if (event.target.offsetHeight + event.target.scrollTop >= event.target.scrollHeight) {
+      this.scrollService.scolledToBottom$.next();
+    }
+}
 }
--- clients/customer-client/src/app/layouts/menu-items/menu-items.component.ts
+++ clients/customer-client/src/app/layouts/menu-items/menu-items.component.ts
@@ -1,44 +1,63 @@
-import { Component, OnDestroy, OnInit, signal } from '@angular/core';
+import {
+  Component,
+  OnDestroy,
+  OnInit,
+  signal,
+} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MenuItemService } from '../../services/menu-item.service';
 import { IMenuItemCard } from '../../interfaces/menu-item.interface';
 import { Subscription } from 'rxjs';
 import { MenuItemCardComponent } from '../../components/menu-item-card/menu-item-card.component';
+import { ScrollService } from '../main/scroll.service';
 
 @Component({
   selector: 'app-menu-items',
   standalone: true,
-  imports: [
-    MenuItemCardComponent
-  ],
+  imports: [MenuItemCardComponent],
   templateUrl: './menu-items.component.html',
-  styleUrl: './menu-items.component.css'
+  styleUrl: './menu-items.component.css',
 })
 export class MenuItemsComponent implements OnInit, OnDestroy {
   restaurantId?: number;
   menuItems = signal<IMenuItemCard[]>([]);
 
   menuItemSub?: Subscription;
+  scrollSub?: Subscription;
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private menuItemService: MenuItemService
+    private menuItemService: MenuItemService,
+    private scrollSerice: ScrollService
   ) {}
 
   ngOnInit(): void {
     this.getMenuItems();
+    this.onScrollToBottom();
   }
 
   getMenuItems() {
-    this.restaurantId = parseInt(this.activatedRoute.snapshot.params['restaurantId']);
+    this.restaurantId = parseInt(
+      this.activatedRoute.snapshot.params['restaurantId']
+    );
     if (!this.restaurantId) return;
     console.log(this.restaurantId);
-    this.menuItemSub = this.menuItemService.getRestaurantMenuItems(this.restaurantId).subscribe({
-      next: menuItems => this.menuItems.set(menuItems)
+    this.menuItemSub = this.menuItemService
+      .getRestaurantMenuItems(this.restaurantId)
+      .subscribe({
+        next: (menuItems) => this.menuItems.set(menuItems),
+      });
+  }
+
+  onScrollToBottom() {
+    this.scrollSub = this.scrollSerice.scolledToBottom$.subscribe({
+      next: _ => console.log('Scrollao si do dna')
     });
   }
 
+
   ngOnDestroy(): void {
     this.menuItemSub?.unsubscribe();
+    this.scrollSub?.unsubscribe();
   }
 }

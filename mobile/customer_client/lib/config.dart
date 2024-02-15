2cba0d39b23ef6478c163d9db37f5b1c5df7371c
zuna21 <adir.zunic@gmail.com>
2024-02-15 20:11:39 +0100

    updated config dart

--- mobile/customer_client/lib/config.dart
+++ mobile/customer_client/lib/config.dart
@@ -1,4 +1,4 @@
 class AppConfig {
-  // static const String baseUrl = "work.noshnexus.com";
-  static const String baseUrl = "192.168.0.108:5000";
+  static const String baseUrl = "work.noshnexus.com";
+  // static const String baseUrl = "192.168.0.108:5000";
 }
